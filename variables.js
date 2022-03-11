15319b215f803ac235905d62a18ed727ad00efcb
Andrey Shpak <anshpak.by@gmail.com>
2022-03-11 23:48:15 +0300

    Add solution to variables task and rename html file

---
+++ variables.js
@@ -0,0 +1,15 @@
+"use strict"
+// first task
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin);
+// second task
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+// third task
+const BIRTHDAY = '18.04.1982'; // here I use capitals because the value is known before script performing
+
+const age = someCode(BIRTHDAY); // here the constant is set by lowercase letters because it is "less constant" then the first one
+// it is calculated during the process
