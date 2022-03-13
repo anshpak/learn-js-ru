03d40713422d81a004e9d95f801a91ba5d880e8b
Andrey Shpak <anshpak.by@gmail.com>
2022-03-13 14:57:29 +0300

    Add solution to fix-addition task

---
+++ fix-addition.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let a = prompt("First num?", 1);
+let b = prompt("Second num?", 2);
+
+alert(+a + +b); // 12
+
+/*
+or just like that:
+let a = +prompt("First num?", 1);
+let b = +prompt("Second num?", 2);
+*/
