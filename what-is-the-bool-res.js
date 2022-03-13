e87680d1b01b2f58687af41ef74769cc623988ed
Andrey Shpak <anshpak.by@gmail.com>
2022-03-13 21:21:36 +0300

    Add solution to what-is-the-bool-res task

---
+++ what-is-the-bool-res.js
@@ -0,0 +1,15 @@
+"use strict"
+
+5 > 4; // true
+
+"apple" > "pineapple"; //false, "a" < "p"
+
+"2" > "12"; // true, "2" > "1"
+
+undefined == null; //true
+
+undefined === null; //false
+
+null == "\n0\n"; //false
+
+null === +"\n0\n"; //false
