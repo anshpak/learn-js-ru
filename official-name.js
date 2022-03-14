621b66a5a10052f4b3adcd4b54f5391c444daa32
Andrey Shpak <anshpak.by@gmail.com>
2022-03-14 17:06:45 +0300

    Add new solution to task: official-name

---
+++ official-name.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let name = prompt('What is official JS name?', '');
+if(name === "ECMAScript") {
+    alert('You\'re right!');
+} else 
+    {
+        alert('Dont you know? It\'s ECMAScript');
+    }
+
+/*
+    let name = prompt('What is official JS name?', '');
+    (name === "ECMAScript") ? alert('You\'re right!') : alert('Dont you know? It\'s ECMAScript');
+*/
