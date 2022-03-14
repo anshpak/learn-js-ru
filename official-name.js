5ee6b044adc1be73728cbced6ec631d3478bd86e
Andrey Shpak <anshpak.by@gmail.com>
2022-03-14 17:09:50 +0300

    feat: add warning to official-name task

--- official-name.js
+++ official-name.js
@@ -8,7 +8,7 @@ if(name === "ECMAScript") {
         alert('Dont you know? It\'s ECMAScript');
     }
 
-/*
+/* warning: it's better to not use ? in such situations because it's dedicated not for constructions of instructions
     let name = prompt('What is official JS name?', '');
     (name === "ECMAScript") ? alert('You\'re right!') : alert('Dont you know? It\'s ECMAScript');
 */
