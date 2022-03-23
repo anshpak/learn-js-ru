088335d8ce78a89f176777f124a87609f749dda2
Andrey Shpak <anshpak.by@gmail.com>
2022-03-23 22:51:10 +0300

    complete task: if-to-switch

--- if-to-switch.js
+++ if-to-switch.js
@@ -1,1 +1,14 @@
 'use strict'
+
+const number = +prompt('Enter the num between 0 и 3', '');
+
+switch (number) {
+    case 0: alert('0');
+    break;
+    case 1: alert('1');
+    break;
+    case 2:
+    case 3:
+        alert('2 or 3');
+    break;
+}
